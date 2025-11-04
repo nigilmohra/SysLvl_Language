4fa625631460195f31c2ae93ba98f890a5771dd1
Nigil Mohra <82286406+nigilmohra@users.noreply.github.com>
2025-11-04 20:30:53 +0530

    Add files via upload

---
+++ C/Examples/Ex01A_HelloWorld/Hello.c
@@ -0,0 +1,10 @@
+// Implementation File - defines how those functions work.
+
+#include <stdio.h>
+// User-Defined Header
+#include "Hello.h"
+
+// Function Definion
+void sayHello(void) {
+  printf("Hello, World!\n");
+}
