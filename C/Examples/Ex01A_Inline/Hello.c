b78468d74cbc654ec0b6f2ecc3f6a4d0a9899d20
Nigil Mohra <82286406+nigilmohra@users.noreply.github.com>
2025-11-04 20:29:20 +0530

    Add files via upload

---
+++ C/Examples/Ex01A_Inline/Hello.c
@@ -0,0 +1,9 @@
+// Implementation File
+
+// User-Defined Header
+#include "Hello.h"
+
+// Function Definion
+void printHelloNormal() {
+  printf("Hello, Normal World!\n");
+}
---
+++ C/Examples/Ex01A_Inline/Main.c
@@ -0,0 +1,11 @@
+// Main Program 
+
+#include "Hello.h"
+
+int main(void) {
+  // Call the Inline Function
+  printHelloInline();
+  // Call the Normal Function
+  printHelloNormal();
+  return 0;
+}
